87333817ba7ac00dfff4ff71f8f2845be8874921
NonoWino <vnovickov32@gmail.com>
2025-08-10 23:49:16 +0300

    Added func min num

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print("first commit")
-print("Branch test")
+def find_smallest_int(arr):
+    if len(arr) > 0:
+        min_num = arr[0]
+        for element in arr:
+            if element < min_num:
+                min_num = element
+        return min_num
+    else:
+        return None
+
+
+
+
+
+
+
+print(find_smallest_int( [34, 15, 88, 2, -1] ))
